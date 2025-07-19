b7d2a99f0dcd0aeac5bd56ab5535181521dc41ae
looney-blooper <mithunarulmani017@gmail.com>
2025-07-19 14:38:17 +0530

    vec_db_created

--- rag_db_create.py
+++ rag_db_create.py
@@ -1,7 +1,5 @@
-from langchain_google_genai import GoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
-from langchain.schema import HumanMessage, AIMessage, SystemMessage
-from langchain.schema.output_parser import StrOutputParser
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 import os
@@ -9,15 +7,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_dir,"hp1.txt")
+file_path = os.path.join(current_dir,"books")
 presistent_dir = os.path.join(current_dir,"chroma_db")
 
 if not os.path.exists(presistent_dir):
     if not os.path.exists(file_path):
         raise FileNotFoundError("no dataset is found")
     
-    loader = TextLoader(file_path)
-    documents = loader.load()
+    books = [ doc for doc in os.listdir(file_path) if doc.endswith(".txt")]
+
+    documents = []
+    for book in books:
+        book_path = os.path.join(file_path,book)
+        loader = TextLoader(book_path)
+        book_docs=loader.load()
+        for doc in book_docs:
+            doc.metadata = {"source":book}
+            documents.append(doc)
 
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
     docs = text_splitter.split_documents(documents)
